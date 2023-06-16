71c19b5e92b8df9be96dcd301d7915d29683a3c3
iman <iman.ghavasieh@gmail.com>
2023-06-16 14:57:59 +0330

    feat(create-user.dto.ts): add more validations for username and password
    Now username can only be lowercase. Password should be between 6 to 20 characters and should include
    at least one lowercase letter, one uppercase letter, one numeric digit, and one special character.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,28 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsLowercase, IsNotEmpty, IsString, Matches, MaxLength, MinLength,
+} from 'class-validator';
 
 export class CreateUserDTO {
   @IsString()
   @IsNotEmpty()
+  @IsLowercase()
     username: string;
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(6, {
+    message: 'minLength-{"ln":6,"count":6}',
+  })
+  @MaxLength(20, {
+    message: 'maxLength-{"ln":20,"count":20}',
+  })
+  @Matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,20}$/,
+    {
+      message:
+        'password should contain at least one lowercase letter, one uppercase letter,'
+        + 'one numeric digit, and one special character',
+    },
+  )
     password: string;
 }
